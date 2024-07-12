2e2717234e0bd784083bfb41fb4d72c05e9edc56
ShahriarHossainDev <shahriar.hossain.dev@gmail.com>
2024-07-12 17:17:17 +0600

    Initial Commit

---
+++ LoveRate/LoveRateApp.swift
@@ -0,0 +1,20 @@
+//
+//  LoveRateApp.swift
+//  LoveRate
+//
+//  Created by Shishir_Mac on 12/7/24.
+//
+
+import SwiftUI
+
+@main
+struct LoveRateApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
